0efd220053ec46919a7b6b513514388846d6f68f
Fabio Checconi <fchecconi@gmail.com>
2011-11-21 17:54:38 -0500

    sched: IRMOS-only changes, EDF stats and multi-CPU interface
    This patch is not intended for mainline submission, it contains extensions
    developed in the context of the IRMOS EU project:
    
      - export EDF statistics and parameters;
      - allow configuring independently runtime and period on multiple CPUs.

--- kernel/sched_rt.c
+++ kernel/sched_rt.c
@@ -1108,6 +1108,26 @@ static inline int rt_se_prio(struct sched_rt_entity *rt_se)
 	return rt_task_of(rt_se)->prio;
 }
 
+#ifdef CONFIG_RT_GROUP_STATS
+static inline void sched_rt_stats_exceeded(struct rt_rq *rt_rq)
+{
+	rt_rq->rt_stats_exceeded++;
+}
+
+static inline void sched_rt_stats_reset(struct rt_rq *rt_rq)
+{
+	rt_rq->rt_stats_reset++;
+}
+#else
+static inline void sched_rt_stats_exceeded(struct rt_rq *rt_rq)
+{
+}
+
+static inline void sched_rt_stats_reset(struct rt_rq *rt_rq)
+{
+}
+#endif
+
 static int sched_rt_runtime_exceeded(struct rt_rq *rt_rq)
 {
 	u64 runtime = sched_rt_runtime(rt_rq);
@@ -1135,6 +1155,7 @@ static int sched_rt_runtime_exceeded(struct rt_rq *rt_rq)
 		 */
 		if (likely(rt_b->rt_runtime)) {
 			rt_rq->rt_throttled = 1;
+			sched_rt_stats_exceeded(rt_rq);
 			start_rt_period_timer(rt_rq);
 		} else {
 			/*
@@ -1414,6 +1435,8 @@ static void rt_rq_update_deadline(struct rt_rq *rt_rq)
 update:
 		rt_rq->rt_deadline = rq->clock + period;
 		rt_rq->rt_time -= min(runtime, rt_rq->rt_time);
+
+		sched_rt_stats_reset(rt_rq);
 
 		/*
 		* Be sure to return a runqueue that can execute, if it
